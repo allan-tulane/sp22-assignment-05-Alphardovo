6410f330838e2f905565bfce0ad2f7fd5864dd21
Alphardovo <98483817+Alphardovo@users.noreply.github.com>
2022-04-29 03:56:59 +0000

    ass5

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Assignment 5
 ## Answers
 
-**Name:**_________________________
+**Name:**____Maria Chen_____________________
 
 
 Place all written answers from `assignment-05.md` here for easier grading.
@@ -11,14 +11,44 @@ Place all written answers from `assignment-05.md` here for easier grading.
 
 
 - **1a.**
+First find the smallest k that satisfies 2^k >= N. Then traverse from k to 0, and compare N with 2^i each time. If N>=2^i, then reduce N by 2^i and record 2^i, the answer is all coins recorded.
+Proof of legitimacy: Any number can be represented in binary form, and all the coins of binary power in this problem exist, so N dollars can definitely be made up. At the same time, after arbitrarily selecting a legal conis, there must also be a legal scheme for the newly generated numbers. When greedy selection from large to small, if a certain amount of coins that can be collected is not selected, more coins need to be accumulated in order to collect the corresponding amount later. So the above greedy algorithm is correct.
 
 
+- **1b.**
+Work: O(logN).
+Span: O(logN)
+Start from 1 and find the smallest k to satisfy 2^k>=N, and the time overhead is O(logN). Then loop through k to 0 to select coins, the time overhead is O(logN), so the work is O(logN),span is O(logN)
 
 
+- **2a.**
+  Suppose there are a total of 3 coins, the denominations are {2, 6, 7}, and you need to find $8. According to the above algorithm, first select 7, then N becomes 1, and then it cannot be selected. But actually 2 and 6 should be chosen. The reason for the error of the greedy algorithm is that the greedy algorithm only considers the local optimal solution, and does not consider the optimal solution from the overall point of view. When 7 is found, 7 is directly selected, but because coins cannot be split and do not exist coins with a denomination of 1, so there is an error.
 
-
-
+  
 - **2b.**
+def func(N, coins, cnt, ans, dp):
+    if N == 0:
+        if ans[0] > cnt:
+            ans[0] = cnt
+        return cnt
+    if len(coins) == 0:
+        return
+    if dp[N][len(coins)] != 0:
+        return dp[N][len(coins)]
+    for i in range(N/coins[0]+1):
+        value = func(N-i*coins[0], coins[1:], cnt + i, ans, dp)
+        if value is None:
+            if dp[N][len(coins)] == 0 or dp[N][len(coins)] is None:
+                dp[N][len(coins)] = None
+            else:
+                dp[N][len(coins)] = min(dp[N][len(coins)], value)
+        else:
+            if dp[N][len(coins)] == 0 or dp[N][len(coins)] is None:
+                dp[N][len(coins)] = value
+            else:
+                dp[N][len(coins)] = min(dp[N][len(coins)], value)
+    return dp[N][len(coins)]
 
-
+Work: O(N*K).
+Span: O(k)
 
--- main.py
+++ main.py
@@ -12,25 +12,84 @@ def MED(S, T):
         return(len(S))
     else:
         if (S[0] == T[0]):
-            return(MED(S[1:], T[1:]))
+            return min(MED(S[1:],T)+1, min(MED(S, T[1:])+1, MED(S[1:], T[1:])))
         else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+            return min(MED(S[1:],T)+1, min(MED(S, T[1:])+1, MED(S[1:], T[1:])+1))
 
 
 def fast_MED(S, T, MED={}):
-    # TODO -  implement memoization
-    pass
+    if (S, T) in MED:
+        return MED[(S, T)]
+    if (S == ""):
+        return(len(T))
+    elif (T == ""):
+        return(len(S))
+    else:
+        if (S[0] == T[0]):
+            MED[(S,T)] = min(fast_MED(S[1:],T,MED)+1, min(fast_MED(S, T[1:],MED)+1, fast_MED(S[1:], T[1:],MED)))
+            return MED[(S,T)]
+        else:
+            MED[(S, T)] = min(fast_MED(S[1:], T, MED)+1, min(fast_MED(S, T[1:], MED) + 1, fast_MED(S[1:], T[1:], MED)+1))
+            return MED[(S, T)]
 
 def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
+    if (S, T) in MED:
+        return MED[(S, T)][0], MED[(S, T)][1]
+    if (S == ""):
+        MED[(S,T)] = ("-"*len(T),T, len(T))
+        # print(MED[(S,T)])
+        return "-"*len(T),T
+    elif (T == ""):
+        MED[(S, T)] = (S, "-"*len(S), len(S))
+        return S, "-"*len(S)
+    else:
+        if (S[0] == T[0]):
+            a = fast_align_MED(S[1:], T, MED)
+            b = fast_align_MED(S, T[1:], MED)
+            c = fast_align_MED(S[1:], T[1:], MED)
+            cnt1 = MED[(S[1:], T)][2] + 1
+            cnt2 = MED[(S, T[1:])][2] + 1
+            cnt3 = MED[(S[1:], T[1:])][2]
+            a = (S[0]+a[0], "-"+a[1])
+            b = ("-"+b[0], T[0]+b[1])
+            c = (S[0]+c[0], T[0]+c[1])
+            if cnt3 <= cnt2 and cnt3 <= cnt1:
+                MED[(S, T)] = (c[0], c[1], cnt3)
+            elif cnt2 <= cnt3 and cnt2 <= cnt1:
+                MED[(S, T)] = (b[0], b[1], cnt2)
+            else:
+                MED[(S, T)] = (a[0], a[1], cnt1)
+            return MED[(S, T)][0], MED[(S, T)][1]
+        else:
+            a = fast_align_MED(S[1:], T, MED)
+            b = fast_align_MED(S, T[1:], MED)
+            c = fast_align_MED(S[1:], T[1:], MED)
+            cnt1 = MED[(S[1:], T)][2] + 1
+            cnt2 = MED[(S, T[1:])][2] + 1
+            cnt3 = MED[(S[1:], T[1:])][2] + 1
+            a = (S[0]+a[0], "-" + a[1])
+            b = ("-" + b[0], T[0]+b[1])
+            c = (S[0] + c[0], T[0] + c[1])
+            # if S[0] == 'u' and len(S) == len(T) + 1:
+            #     print(a,b,c)
+            if cnt3 <= cnt2 and cnt3 <= cnt1:
+                MED[(S, T)] = (c[0], c[1], cnt3)
+            elif cnt2 <= cnt3 and cnt2 <= cnt1:
+                MED[(S, T)] = (b[0], b[1], cnt2)
+            else:
+                MED[(S, T)] = (a[0], a[1], cnt1)
+            return MED[(S, T)][0], MED[(S, T)][1]
 
 def test_MED():
     for S, T in test_cases:
         assert fast_MED(S, T) == MED(S, T)
-                                 
+
 def test_align():
     for i in range(len(test_cases)):
         S, T = test_cases[i]
-        align_S, align_T = fast_align_MED(S, T)
+        mst = {}
+        align_S, align_T = fast_align_MED(S, T, mst)
         assert (align_S == alignments[i][0] and align_T == alignments[i][1])
+
+# test_MED()
+test_align()
